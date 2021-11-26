5a5488f12537a7470c2a7359da020acc2ab27a17
TJ Tahmassebi <tjtahmas@gmail.com>
2021-11-26 17:56:09 -0500

    still working on routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const { Tag, Product, ProductTag, Category } = require('../../models');
 
 // The `/api/tags` endpoint
 
@@ -47,12 +47,78 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+/*
+//Didn't make this one async because I matched it to the Product example
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((tag) => {
+      // find all associated products from ProductTag
+      return ProductTag.findAll ({ where: { product_id: req.params.id} });      
+    })
+    .then((productTags) => {
+      // get list of current product_ids
+      const productTagIds = productTags.map(({ product_id }) => product_id);
+      // create filtered list of new product_ids
+      const newProductTags = req.body.productIds
+        .filter((product_id) => !productTagIds.includes(product_id))
+        .map((product_id) => {
+          return {
+            tag_id: req.params.id,
+            product_id,
+          };
+        });
+        // figure out which ones to remove
+        const productTagsToRemove = productTags
+          .filter(({ product_id}) => !req.body.productIds.includes(product_id))
+          .map(({ id }) => id);
+
+        return Promise.all([
+          ProductTag.destroy({ where: { id: productTagsToRemove} }),
+          ProductTag.bulkCreate(newProductTags),
+        ]);
+    })
+    .then((updatedProductTags) => res.json(updatedProductTags))
+    .catch((err) => {
+      //console.log(err)
+      res.status(400).json(err);
+    });
+  });
+  */
+
+router.put('/:id', async (req, res) => {
+  //update a tag by its `id` value
+  try {
+    const tagData = await Category.updateOne({
+      id: req.params.id
+    })
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with that id'});
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
